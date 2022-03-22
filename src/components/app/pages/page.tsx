8cf3237340d93f3df243163328b3d494109bf2f2
Ezekiel Pierson <PiersonBro@users.noreply.github.com>
2022-03-22 10:08:01 -0700

    Explcitly set the type of the Page Component
    This fixes a TypeScript error that VSCode shows.

--- src/components/app/pages/page.tsx
+++ src/components/app/pages/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-const Page = ({ children }) => {
+const Page: React.FC = ({ children }) => {
 	return (
 		<React.Fragment>
 			<main style={{ width: "100%", height: "100%" }}>{children}</main>
