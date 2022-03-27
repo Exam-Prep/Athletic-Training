f4907b3f57956e52bfa1cf897271b65ef6b58c72
Alavish <achristopher22@my.whitworth.edu>
2022-03-26 21:56:42 -0700

    Component additions

--- src/components/app/pages/questions/index.tsx
+++ src/components/app/pages/questions/index.tsx
@@ -1,10 +1,15 @@
 /** @format */
 
 import React from "react";
+import QuestionToolBox from "../../../questions-toolbar";
 
 import Page from "../page";
 
 import styles from "./styles.scss";
+import QuestionsToolBar from "../../../questions-toolbar";
+import QuestionToolBar from "../../../questions-toolbar";
+
+
 
 const Questions = () => {
 	return (
@@ -12,6 +17,9 @@ const Questions = () => {
 			<div className={styles.questionsBox}></div>
 			<div className={styles.questionsToolBar}></div>
 			<div className={styles.titleBar}></div>
+			<div className={styles.questionsToolBar}>
+					<QuestionToolBar />
+			</div>
 		</Page>
 	);
 };
