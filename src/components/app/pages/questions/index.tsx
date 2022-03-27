a973a31a4af6bc1045d8d45815998e617b6883ab
Ezekiel Pierson <PiersonBro@users.noreply.github.com>
2022-03-26 17:25:54 -0700

    Add the ability to name, and upload exams to firebase. Also add the ability to append multiple choice questions to existing exams.
    The JSON unwrapping logic requires the use of the Map data structure.
    Turn on a TypeScript compiler flag, downlevelIteration, to allow for-loops to easily interact with Map objects.

--- src/components/app/pages/questions/index.tsx
+++ src/components/app/pages/questions/index.tsx
@@ -1,14 +1,16 @@
 /** @format */
-
 import React from "react";
 import Page from "../page";
 import styles from "./styles.scss";
 import QuestionToolBar from "../../../questions-toolbar";
+import QuestionWriterUI from "../../../questions-box";
 
 const Questions = () => {
 	return (
 		<Page>
-			<div className={styles.questionsBox}></div>
+			<div className={styles.questionsBox}>
+				<QuestionWriterUI />
+			</div>
 			<div className={styles.questionsToolBar}></div>
 			<div className={styles.titleBar}></div>
 			<div className={styles.questionsToolBar}>
