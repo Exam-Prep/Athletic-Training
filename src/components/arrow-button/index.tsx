bc066ef4fa8d33dc0544e84ccc4f9bf6511c0756
Tyler J. Gamlem <tgamlem22@my.whitworth.edu>
2022-04-04 09:34:39 -0700

    add arrow button for clicking to next and previous questions

---
+++ src/components/arrow-button/index.tsx
@@ -0,0 +1,43 @@
+/** @format */
+
+import React from "react";
+import styles from "./styles.scss";
+
+interface ArrowButtonProps {
+	onClick: () => void;
+	rotate: boolean;
+	text: string;
+}
+
+const ArrowButton: React.FunctionComponent<ArrowButtonProps> = ({
+	onClick,
+	rotate,
+	text,
+}) => {
+	return (
+		<button
+			className={`${styles.transparent} ${
+				rotate ? styles.leftArrow : null
+			}`}
+			onClick={onClick}
+		>
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				width='600'
+				height='600'
+				viewBox='0 0 640 640'
+				xmlSpace='preserve'
+			>
+				<path
+					fill='#0158ae'
+					vectorEffect='non-scaling-stroke'
+					transform='matrix(6.26 0 0 6.26 75.02 65.64)'
+					d='M41.17 72.5 80 40 41.17 7.5v20.605H0v23.847h41.17V72.5z'
+				/>
+			</svg>
+			<span className={styles.text}>{text}</span>
+		</button>
+	);
+};
+
+export default ArrowButton;
