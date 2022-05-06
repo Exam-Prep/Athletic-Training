ce54f0f4d26ff5740b8111e77732b42385b2add8
Tyler J. Gamlem <tgamlem22@my.whitworth.edu>
2022-05-06 16:29:14 -0700

    initial production build

--- declaration.d.ts
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-declare module "*.scss";
-
-declare module "react-dom/client" {
-	// typing module default export as `any` will allow you to access its members without compiler warning
-	const createRoot: any;
-	export { createRoot };
-}
